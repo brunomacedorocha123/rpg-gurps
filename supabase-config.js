349f87a9734e012280f7184aa873faead938fb85
brunomacedorocha123 <brunomacedorocha123@gmail.com>
2025-12-21 11:51:32 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
+// supabase-config.js - Coloque na mesma pasta que index.html
 const SUPABASE_URL = 'https://mdyohqtvswunkhefbrkr.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keW9ocXR2c3d1bmtoZWZicmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyODA4NzYsImV4cCI6MjA4MTg1Njg3Nn0.bfc4sh8kDNhAXf9bRAlDuB0lGTfQIWNl6Sju1LViE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keW9ocXR2c3d1bmtoZWZicmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyODA4NzYsImV4cCI6MjA4MTg1Njg3Nn0.bfc4sh8kDNhAXf9b1eRAlDuB0lGTfQIWNl6Sju1LViE';
 
-const { createClient } = supabase;
-window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = supabase;
