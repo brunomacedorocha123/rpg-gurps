b345dfce9bccad100fdd79158ba7fb93fa324f45
brunomacedorocha123 <brunomacedorocha123@gmail.com>
2025-12-21 00:37:34 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const supabaseUrl = 'https://mdyohqtvswunkhefbrkr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1keW9ocXR2c3d1bmtoZWZicmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyODA4NzYsImV4cCI6MjA4MTg1Njg3Nn0.bfc4sh8kDNhAXf9b1eRAlDuB0lGTfQIWNl6Sju1LViE';
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
